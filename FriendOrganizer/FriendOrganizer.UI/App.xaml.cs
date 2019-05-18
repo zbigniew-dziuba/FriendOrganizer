a5702e43c607c24aa452e792345b6a1a3c6ceb13
Zbigniew Dziuba <zbigniew.dziuba91@gmail.com>
2019-05-19 00:00:58 +0200

    Add base UI layer.

--- FriendOrganizer/FriendOrganizer.UI/App.xaml.cs
+++ FriendOrganizer/FriendOrganizer.UI/App.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Autofac;
+using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.Startup;
+using FriendOrganizer.UI.ViewModel;
 using System.Windows;
 
 namespace FriendOrganizer.UI
@@ -13,5 +11,13 @@ namespace FriendOrganizer.UI
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var bootstrapper = new Bootstrapper();
+            var container = bootstrapper.Bootstrap();
+
+            var mainWindow = container.Resolve<MainWindow>();
+            MainWindow.Show();
+        }
     }
 }
---
+++ FriendOrganizer/FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,20 @@
+﻿using FriendOrganizer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            yield return new Friend { FirstName = "Thomas", LastName = "Huber" };
+            yield return new Friend { FirstName = "Andreas", LastName = "Boehler" };
+            yield return new Friend { FirstName = "Julia", LastName = "Huber" };
+            yield return new Friend { FirstName = "Chrissi", LastName = "Egin" };
+        }
+    }
+}
